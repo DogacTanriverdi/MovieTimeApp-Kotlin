60fa2bd8e10f44336120176cb9303ae1d653ebe0
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-10-07 10:15:45 +0300

    Bottom Nav Bar created & Navigation setup completed

--- app/src/main/java/com/dogactnrvrdi/movietime/activities/MainActivity.kt
+++ app/src/main/java/com/dogactnrvrdi/movietime/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package com.dogactnrvrdi.movietime.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.dogactnrvrdi.movietime.R
+import com.dogactnrvrdi.movietime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bottomNavBar = binding.bottomNavBar
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragmentContainerView
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavBar, navController)
     }
 }
